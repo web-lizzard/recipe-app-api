33a349504e2edd7d5d53bd56d292bac754c5812a
web-lizzard <czarnous.adrian@gmail.com>
2023-03-10 19:10:27 +0100

    customize django admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,12 +1,40 @@
 from core import models
 from django.contrib import admin  # noqa
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as translate
 
 
 # Register your models here.
 class UserAdmin(BaseUserAdmin):
     ordering = ["email"]
-    list_display = ["email", "name"]
+    list_display = ["email", "name", "id"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            translate("Permissions"),
+            {"fields": ("is_active", "is_staff", "is_superuser")},
+        ),
+        (translate("Important dates"), {"fields": ("last_login",)}),
+    )
+
+    readonly_fields = ["last_login"]
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "name",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -25,3 +25,19 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(response, self.user.email)
         self.assertContains(response, self.user.name)
+
+    def test_edit_user_page(self):
+        """Test if user edit page works on admin panel"""
+
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test if creating user page works"""
+
+        url = reverse("admin:core_user_add")
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
